2d12f921d696ea6258b6191a3b58fdb1d53f0411
THARAK HEGDE <b22mt044@iitj.ac.in>
2024-06-20 15:34:31 +0530

    4uretrkt
    iertueirujerlkndfgierg jer

--- frontend/src/Publications/Publs_widgets/PublsMain.jsx
+++ frontend/src/Publications/Publs_widgets/PublsMain.jsx
@@ -8,7 +8,12 @@ export default function PublsMain(publications) {
                 publications.publications.map((elem)=>{
                     return <PubCard publication={elem}/>
                 })
+
+
+
             }
+            <h1> Hi there this is RAID</h1>
+            <h3>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis, quibusdam id obcaecati similique ea perspiciatis iure dolorum iste et recusandae enim. Eius nihil a neque adipisci error quaerat nulla provident.</h3>
         </div>
     );
 }
