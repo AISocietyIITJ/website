ca6879c51e393ba969a3fb0792eab20eee4808f1
Raghuveer Kulkarni <147660422+RaghuveeRR07@users.noreply.github.com>
2024-10-28 04:40:50 +0530

    updated navbar for mobile

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes, useRoutes} from "react-router-dom";
 import './App.css'
-import Navbar from './NavbarNFooter/Navbar3.jsx';
+import Navbar from './NavbarNFooter/Navbar4.jsx';
 import Footer from './NavbarNFooter/Footer.jsx';
 import Home from './Home/Home2.jsx';
 import Project from './Projects/Project.jsx';
